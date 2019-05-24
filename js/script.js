bc7ff2ecc117243733cf39d583be581eebe9d766
El Sadate <aelsadate@gmail.com>
2019-05-24 09:27:00 -0400

    update

--- js/script.js
+++ js/script.js
@@ -9,26 +9,26 @@ var pressImage = document.getElementById('press-img');
 var menu = document.getElementsByClassName('menu-item');
 
 for (var i = 0; i < menu.length; i++) {
-  menu[i].addEventListener("click", function() {
+  menu[i].addEventListener("mouseover", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
 }
 
-aboutButton.addEventListener("click", function() {
+aboutButton.addEventListener("mouseover", function() {
   aboutImage.style.display = "initial";
   musicImage.style.display = "none";
   pressImage.style.display = "none";
 })
 
-pressButton.addEventListener("click", function() {
+pressButton.addEventListener("mouseover", function() {
   aboutImage.style.display = "none";
   musicImage.style.display = "none";
   pressImage.style.display = "initial";
 })
 
-musicButton.addEventListener("click", function() {
+musicButton.addEventListener("mouseover", function() {
   aboutImage.style.display = "none";
   musicImage.style.display = "initial";
   pressImage.style.display = "none";
@@ -38,8 +38,46 @@ const albumCoverOne = document.getElementById('album-one');
 const albumCoverTwo = document.getElementById('album-two');
 
 
-function myFunction() {
+function showPlaylistOne() {
   var x = document.getElementById("music-list-one");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showPlaylistTwo() {
+  var x = document.getElementById("music-list-two");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// Hide and show Band members info
+
+function showOne() {
+  var x = document.getElementById("info-one");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showTwo() {
+  var x = document.getElementById("music-list-two");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showPlaylistThree() {
+  var x = document.getElementById("music-list-two");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
